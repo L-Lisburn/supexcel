7012e18720b83c235ac483b33823835f06563ec7
梁嘉辉 <784275493@qq.com>
2021-01-13 17:15:26 +0800

    pattern

--- src/components/excel/excel.vue
+++ src/components/excel/excel.vue
@@ -1,42 +1,80 @@
 <template>
     <div class="exl-container">
         <table class="exl-table">
-            <tr class="exl-table-title">
-                <td
-                    class="exl-td"
-                    v-for="(header , index) in head"
+            <thead>
+                <tr v-if="pattern">
+                    <td class="exl-row-pattern"></td>
+                    <td
+                        class="exl-col-pattern"
+                        v-for="(col , index) in columns[0]"
+                        :key="index"
+                        :colspan="col.colspan === undefined ? '' : col.colspan"
+                        :style="{width : col.width === undefined ? '' : col.width + 'px'}"
+                    >
+                        <span v-html="_exl_init(index)"></span>
+                    </td>
+                </tr>
+                <tr
+                    v-for="(columns , index) in columns"
                     :key="index"
-                    :style="{width : header.width === undefined ? '' : header.width + 'px'}"
                 >
-                    <span class="exl-header">{{header.name}}</span>
-                </td>
-            </tr>
-            <tr>
-            </tr>
-            <tr>
-                <td
-                    class="exl-td"
-                    v-for="i in head.length"
-                    :key="i"
-                >1</td>
-            </tr>
+                    <td
+                        class="exl-row-pattern"
+                        v-if="pattern"
+                    >
+                        {{index + 1}}
+                    </td>
+                    <td
+                        v-for="(col , index) in columns"
+                        :key="index"
+                        :rowspan="col.rowspan === undefined ? '' : col.rowspan"
+                        :colspan="col.colspan === undefined ? '' : col.colspan"
+                        class="exl-td"
+                        :style="{width : col.width === undefined ? '' : col.width + 'px'}"
+                    >
+                        <span class="exl-header">{{col.name}}</span>
+                    </td>
+                </tr>
+            </thead>
         </table>
     </div>
 </template>
 <script>
 export default {
-    props : [
-        'head'
-    ],
+    props : {
+        columns : {
+            type : Array ,
+            required: true
+        },
+        pattern : {
+            type : Boolean ,
+            required : true
+        }
+    },
     data : () => ({
-
+        /* 标准模式开启下：顶部与侧边增加额外的定位栏 */
+        en : ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     }),
+    watch : {
+        columns : {
+            handler : function(n , o){
+                this._exl_init()
+            }
+        }
+    },
     mounted () {
         this._exl_init()
     },
     methods : {
-        _exl_init () {
-            
+        _exl_init (index) {
+            if(this.pattern == true){
+                if(index <= 25){
+                    return this.en[index]
+                }else if(index > 25){
+                    let sub = Math.floor(index/26)
+                    return this.en[index%26] + sub
+                }
+            }
         }
     }
 }
--- src/views/Excel.vue
+++ src/views/Excel.vue
@@ -1,6 +1,7 @@
 <template>
   <Excel
-    :head="head"
+    :columns="columns"
+    :pattern="true"
   />
 </template>
 <script>
@@ -11,48 +12,71 @@ export default {
     Excel
   },
   data : () => ({
-    head : [
-      {
-        name : '姓名' ,
-        solt : 'name' ,
-        width : '400' ,
-        child : [
-            {
-                name : '字'
-            },
-            {
-                name : '号'
-            }
-        ]
-      },
-      {
-        name : '年龄' ,
-        solt : 'age' ,
-      },
-      {
-        name : '性别' ,
-        solt : 'gender'
-      },
-      {
-        name : '电话' ,
-        solt : 'phone'
-      },
-      {
-        name : '身份证' ,
-        solt : 'idcard'
-      },
-      {
-        name : '住址' ,
-        solt : 'address'
-      },
-      {
-        name : '紧急联系人紧急联系人紧急联系人紧急联系人紧急联系人' ,
-        solt : 'urgent'
-      },
-      {
-        name : '紧急联系电话' ,
-        solt : 'urgent_phone'
-      }
+    columns : [
+      [
+        {
+          name : '姓名' ,
+          solt : 'name' ,
+          rowspan : 2 ,
+          colspan : 2 ,
+        },
+        {
+          name : '年龄' ,
+          solt : 'age' ,
+        },
+        {
+          name : '性别' ,
+          solt : 'gender'
+        },
+        {
+          name : '电话' ,
+          solt : 'phone'
+        },
+        {
+          name : '身份证' ,
+          solt : 'idcard'
+        },
+        {
+          name : '住址' ,
+          solt : 'address'
+        },
+        {
+          name : '紧急联系人紧急联系人紧急联系人紧急联系人紧急联系人' ,
+          solt : 'urgent'
+        },
+        {
+          name : '紧急联系电话' ,
+          solt : 'urgent_phone'
+        },
+      ],
+      [
+        {
+          name : '姓名' ,
+          solt : 'name' ,
+        },
+         {
+          name : '姓名' ,
+          solt : 'name' ,
+        },
+         {
+          name : '姓名' ,
+          solt : 'name' ,
+        },
+      ],
+      [
+        {
+          name : '姓名' ,
+          solt : 'name' ,
+        },
+         {
+          name : '姓名' ,
+          solt : 'name' ,
+        },
+         {
+          name : '姓名' ,
+          solt : 'name' ,
+        },
+      ]
     ]
   })
 }
